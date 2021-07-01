2e277fc00386f119aa5c1a5032bbd7057569d7d2
Svaagi <jozefsvagerkom5@gmail.com>
2021-07-01 15:33:31 +0200

    edit CVComponent Názvy a polia v CV

--- src/CVComponent.js
+++ src/CVComponent.js
@@ -3,7 +3,72 @@ import React from 'react'
 function CV() {
   return (
     <div>
-      
+       <section id="about">
+      <div>
+        <h1>Meno</h1>
+        <div>
+          xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+        </div>
+        <p>
+          xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+        </p>
+      </div>
+    </section>
+
+    <section id="experience">
+      <h2>Experience</h2>
+      <div>
+        <div>
+          <h3>Senior Web Developer</h3>
+          <div>Intelitec Solutions</div>
+          <p>
+            xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+          </p>
+        </div>
+        <div>March 2013 - Present</div>
+      </div>
+      <div>
+        <div>
+          <h3>Web Developer</h3>
+          <div>Intelitec Solutions</div>
+          <p>
+           xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+          </p>
+        </div>
+        <div>December 2011 - March 2013</div>
+      </div>
+      <div>
+        <div>
+          <h3>Junior Web Designer</h3>
+          <div>SHOUT!</div>
+          <p>
+            xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+          </p>
+        </div>
+        <div>July 2010 - December 2011</div>
+      </div>
+    </section>
+
+    <section id="education">
+      <h2>Education</h2>
+      <p>
+        xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+      </p>
+    </section>
+
+    <section id="skills">
+      <h2>Skills</h2>
+      <p>
+        xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+      </p>
+    </section>
+
+    <section id="contact">
+      <h2>Interests</h2>
+      <p>
+        xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+      </p>
+    </section>
     </div>
   )
 }

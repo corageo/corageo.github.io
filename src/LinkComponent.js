2922e087098e9ba306b673dbf974b2ac4983574c
Marko Tomčík <markotomcik@gmail.com>
2021-07-03 17:26:40 +0200

    Connected components + testing signed commit

--- src/LinkComponent.js
+++ src/LinkComponent.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import CV from './CVComponent'
 
 function Link() {
   return (
-    <div>
+    <>
       
-    </div>
+    </>
   )
 }
 
--- src/MainComponent.js
+++ src/MainComponent.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import Link from './LinkComponent'
 
 function Main() {
   return (
-    <div>
-      
-    </div>
+    <>
+      <Link />
+    </>
   )
 }
 

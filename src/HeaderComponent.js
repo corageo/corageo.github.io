3bfe69b1c05affb15d15d936811f8b3686909bfc
Marko Tomčík <markotomcik@gmail.com>
2021-07-03 17:29:23 +0200

    Solved warnings

--- src/HeaderComponent.js
+++ src/HeaderComponent.js
@@ -7,7 +7,7 @@ function Header() {
         <nav >
           <ul  className="bg-yellow-300 flex justify-center items-center">
             <li className="px-20">CV - 4 žrebci</li>
-            <li className="px-20"><img src={corageoLogo} /></li>
+            <li className="px-20"><img src={corageoLogo} alt="CORAgeo Logo" /></li>
           </ul>
         </nav>
     </>

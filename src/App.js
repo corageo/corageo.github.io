d608386897db65fe3c7d8ed1ba3cadb21b747f6f
Pluvas159 <maniksamuel159@gmail.com>
2021-07-06 11:05:47 +0200

    first routing

--- src/App.js
+++ src/App.js
@@ -2,14 +2,45 @@ import Header from './HeaderComponent'
 import Main from './MainComponent'
 import Footer from './FooterComponent'
 import CV from './CVComponent'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
+//<Header />
+//      <Main />                            
+//      <Footer />
 function App() {
   return (
-    <>
-      <Header />
-      <Main />
-      <Footer />
-    </>
+    <Router>
+  <Switch>
+    <Route path="/en/home">
+       <div>
+        <Header />
+        <Main />
+        <Footer />
+       </div>
+    </Route>
+    <Route path="/en/manik">
+        <Header />
+        <CV id="1" />
+        <Footer />
+    </Route>
+  </Switch>
+    </Router>
+
+  )
+}
+
+function home(){
+  return(
+    <div>
+  <Header />
+  <Main />
+  <Footer />
+  </div>
   )
 }
 
